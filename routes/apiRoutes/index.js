1148547345d0fd17338282715eebc54352601889
vcaruso <vinnycar0923@gmail.com>
2020-08-03 18:59:46 -0700

    POST api now works

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,28 +2,33 @@ const { notes } = require('../../db/db.json');
 const router = require('express').Router();
 const fs = require("fs");
 const path = require("path");
+var uuid = require('uuid');
 
 router.post('/notes', (req, res) => {
-    var data = req.body;
-    console.log(data);
+    var noteData = req.body;
+    noteData.id = uuid.v1();
+    console.log(noteData);
 
-    const newNote = function () {
-        notes.push(data);
-        fs.writeFileSync(
-            path.join(__dirname, '../../../db/db.json'),
-            JSON.stringify({ animals: animalsArray }, null, 2)
-        );
-        return animal;
-    }
-    const newNoteTest = newNote();
-    res.json(newNoteTest)
+    fs.readFile('db/db.json', function (err, data) {
+        var json = JSON.parse(data);
+        json.push(noteData);
+            if (err) throw err;
+
+        fs.writeFile("db/db.json", JSON.stringify(json), function (err) {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+    })
+
+    res.json(noteData)
 })
 
 router.get('/notes', (req, res) => {
-    let results = fs.readFileSync(__dirname +"../../../db/db.json");
+    let results = fs.readFileSync(__dirname + "../../../db/db.json");
     let parsedNotes = JSON.parse(results);
     console.log(parsedNotes);
     res.json(parsedNotes);
 });
 
 module.exports = router;
+
